4bc8988cdee65012d7342f038557f6cf4aebffee
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-24 19:31:56 -0500

    Feat: Add the MVC flow for a new button on the index page

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -17,6 +17,13 @@ class Merchant::BulkDiscountsController < ApplicationController
       flash[:alert] = "ERROR: Please Don't Leave Any Field Blank"
     end
   end
+
+  def destroy
+    merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.find(params[:id])
+    bulk_discount.destroy
+    redirect_to merchant_bulk_discounts_path(merchant)
+  end
   
   private
   def bulk_discount_params
